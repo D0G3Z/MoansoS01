5f1da9c137b2ee8d0071cfbd90e03cca4f73659b
DiegoSR <diego.sr24@hotmail.com>
2023-03-26 23:32:31 -0500

    Colocamos botones y funcionalidades(CUD) al FormComboBox

--- FormComboBox.Designer.cs
+++ FormComboBox.Designer.cs
@@ -29,7 +29,115 @@ namespace MoansoS01
         /// </summary>
         private void InitializeComponent()
         {
+            this.cmbTarea = new System.Windows.Forms.ComboBox();
+            this.cmbPrioridad = new System.Windows.Forms.ComboBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.btnEstablecer = new System.Windows.Forms.Button();
+            this.lblTarea = new System.Windows.Forms.Label();
+            this.btnNueva = new System.Windows.Forms.Button();
+            this.txtTarea = new System.Windows.Forms.TextBox();
+            this.btnAgregar = new System.Windows.Forms.Button();
+            this.btnEliminar = new System.Windows.Forms.Button();
             this.SuspendLayout();
+            // 
+            // cmbTarea
+            // 
+            this.cmbTarea.Cursor = System.Windows.Forms.Cursors.Default;
+            this.cmbTarea.DisplayMember = "jghjghj";
+            this.cmbTarea.FormattingEnabled = true;
+            this.cmbTarea.Location = new System.Drawing.Point(93, 28);
+            this.cmbTarea.Name = "cmbTarea";
+            this.cmbTarea.Size = new System.Drawing.Size(125, 21);
+            this.cmbTarea.TabIndex = 0;
+            this.cmbTarea.Tag = "";
+            this.cmbTarea.Text = "Elige una opcion";
+            // 
+            // cmbPrioridad
+            // 
+            this.cmbPrioridad.FormattingEnabled = true;
+            this.cmbPrioridad.Location = new System.Drawing.Point(354, 28);
+            this.cmbPrioridad.Name = "cmbPrioridad";
+            this.cmbPrioridad.Size = new System.Drawing.Size(125, 21);
+            this.cmbPrioridad.TabIndex = 1;
+            this.cmbPrioridad.Text = "Elige una opcion";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(36, 31);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(41, 13);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Tarea: ";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(276, 31);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(54, 13);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Prioridad: ";
+            // 
+            // btnEstablecer
+            // 
+            this.btnEstablecer.Location = new System.Drawing.Point(514, 24);
+            this.btnEstablecer.Name = "btnEstablecer";
+            this.btnEstablecer.Size = new System.Drawing.Size(94, 26);
+            this.btnEstablecer.TabIndex = 4;
+            this.btnEstablecer.Text = "Establecer";
+            this.btnEstablecer.UseVisualStyleBackColor = true;
+            this.btnEstablecer.Click += new System.EventHandler(this.btnEstablecer_Click);
+            // 
+            // lblTarea
+            // 
+            this.lblTarea.AutoSize = true;
+            this.lblTarea.Location = new System.Drawing.Point(36, 72);
+            this.lblTarea.Name = "lblTarea";
+            this.lblTarea.Size = new System.Drawing.Size(76, 13);
+            this.lblTarea.TabIndex = 5;
+            this.lblTarea.Text = "Nueva Tarea: ";
+            this.lblTarea.Visible = false;
+            // 
+            // btnNueva
+            // 
+            this.btnNueva.Location = new System.Drawing.Point(614, 23);
+            this.btnNueva.Name = "btnNueva";
+            this.btnNueva.Size = new System.Drawing.Size(94, 26);
+            this.btnNueva.TabIndex = 6;
+            this.btnNueva.Text = "Nueva Tarea";
+            this.btnNueva.UseVisualStyleBackColor = true;
+            this.btnNueva.Click += new System.EventHandler(this.btnNueva_Click);
+            // 
+            // txtTarea
+            // 
+            this.txtTarea.Location = new System.Drawing.Point(118, 69);
+            this.txtTarea.Name = "txtTarea";
+            this.txtTarea.Size = new System.Drawing.Size(146, 20);
+            this.txtTarea.TabIndex = 7;
+            this.txtTarea.Visible = false;
+            // 
+            // btnAgregar
+            // 
+            this.btnAgregar.Location = new System.Drawing.Point(290, 67);
+            this.btnAgregar.Name = "btnAgregar";
+            this.btnAgregar.Size = new System.Drawing.Size(75, 23);
+            this.btnAgregar.TabIndex = 8;
+            this.btnAgregar.Text = "Agregar";
+            this.btnAgregar.UseVisualStyleBackColor = true;
+            this.btnAgregar.Visible = false;
+            this.btnAgregar.Click += new System.EventHandler(this.btnAgregar_Click);
+            // 
+            // btnEliminar
+            // 
+            this.btnEliminar.Location = new System.Drawing.Point(514, 63);
+            this.btnEliminar.Name = "btnEliminar";
+            this.btnEliminar.Size = new System.Drawing.Size(94, 26);
+            this.btnEliminar.TabIndex = 9;
+            this.btnEliminar.Text = "Eliminar";
+            this.btnEliminar.UseVisualStyleBackColor = true;
+            this.btnEliminar.Click += new System.EventHandler(this.btnEliminar_Click);
             // 
             // FormComboBox
             // 
@@ -37,12 +145,35 @@ namespace MoansoS01
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.Color.MediumPurple;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.btnEliminar);
+            this.Controls.Add(this.btnAgregar);
+            this.Controls.Add(this.txtTarea);
+            this.Controls.Add(this.btnNueva);
+            this.Controls.Add(this.lblTarea);
+            this.Controls.Add(this.btnEstablecer);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.cmbPrioridad);
+            this.Controls.Add(this.cmbTarea);
             this.Name = "FormComboBox";
             this.Text = "FormEj1";
+            this.Load += new System.EventHandler(this.FormComboBox_Load);
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
+
+        private System.Windows.Forms.ComboBox cmbTarea;
+        private System.Windows.Forms.ComboBox cmbPrioridad;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button btnEstablecer;
+        private System.Windows.Forms.Label lblTarea;
+        private System.Windows.Forms.Button btnNueva;
+        private System.Windows.Forms.TextBox txtTarea;
+        private System.Windows.Forms.Button btnAgregar;
+        private System.Windows.Forms.Button btnEliminar;
     }
 }
--- FormComboBox.cs
+++ FormComboBox.cs
@@ -16,5 +16,60 @@ namespace MoansoS01
         {
             InitializeComponent();
         }
+
+        private void FormComboBox_Load(object sender, EventArgs e)
+        {
+            cmbTarea.Items.Add("Realizar un proyecto");
+            cmbTarea.Items.Add("Ir al gimnasio");
+            cmbTarea.Items.Add("Comprar un juego");
+
+            cmbPrioridad.Items.Add("Baja");
+            cmbPrioridad.Items.Add("Normal");
+            cmbPrioridad.Items.Add("Alta");
+        }
+
+        private void btnEstablecer_Click(object sender, EventArgs e)
+        {
+            if (cmbTarea.SelectedIndex != -1 && cmbPrioridad.SelectedIndex != -1)
+            {
+                MessageBox.Show("Se estableció correctamente");
+                cmbPrioridad.Text = "Escoja una ocpión";
+                cmbTarea.Text = "Escoja una ocpión";
+            }               
+            else
+                MessageBox.Show("Escoja una opcion en ambos casos");
+        }
+
+        private void btnAgregar_Click(object sender, EventArgs e)
+        {
+            if (txtTarea.Text != "")
+            {
+                cmbTarea.Items.Add(txtTarea.Text);
+                MessageBox.Show("Se agregó correctamente");
+                lblTarea.Visible = false;
+                txtTarea.Visible = false;
+                btnAgregar.Visible = false;
+            }
+                
+            else
+            {
+                MessageBox.Show("El campo no puede estar vacio");
+                txtTarea.Focus();
+            }
+                
+        }
+
+        private void btnNueva_Click(object sender, EventArgs e)
+        {
+            lblTarea.Visible = true;
+            txtTarea.Visible = true;
+            btnAgregar.Visible = true;
+        }
+
+        private void btnEliminar_Click(object sender, EventArgs e)
+        {
+            cmbTarea.Items.Remove(cmbTarea.SelectedItem);
+            cmbTarea.Text = "Escoja una ocpión";
+        }
     }
 }
