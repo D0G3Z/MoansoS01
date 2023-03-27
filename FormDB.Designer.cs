ecbca2709061da3c0f31c8aa3513fd622301bf90
DiegoSR <diego.sr24@hotmail.com>
2023-03-27 12:08:29 -0500

    Editando el FormDB y estableciendo conexion con SqlServer(Pendiente por terminar)

--- FormDB.Designer.cs
+++ FormDB.Designer.cs
@@ -29,12 +29,167 @@ namespace MoansoS01
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.txtNombre = new System.Windows.Forms.TextBox();
+            this.txtAutor = new System.Windows.Forms.TextBox();
+            this.txtCodigo = new System.Windows.Forms.TextBox();
+            this.btnAgregar = new System.Windows.Forms.Button();
+            this.label4 = new System.Windows.Forms.Label();
+            this.chkSi = new System.Windows.Forms.CheckBox();
+            this.chkNo = new System.Windows.Forms.CheckBox();
+            this.btnActualizar = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(27, 159);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.ReadOnly = true;
+            this.dataGridView1.SelectionMode = System.Windows.Forms.DataGridViewSelectionMode.FullRowSelect;
+            this.dataGridView1.Size = new System.Drawing.Size(574, 199);
+            this.dataGridView1.TabIndex = 0;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(24, 19);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(91, 13);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Nombre de Libro: ";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(24, 81);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(46, 13);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Codigo: ";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(24, 49);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(38, 13);
+            this.label3.TabIndex = 3;
+            this.label3.Text = "Autor: ";
+            // 
+            // txtNombre
+            // 
+            this.txtNombre.Location = new System.Drawing.Point(130, 16);
+            this.txtNombre.Name = "txtNombre";
+            this.txtNombre.Size = new System.Drawing.Size(162, 20);
+            this.txtNombre.TabIndex = 4;
+            // 
+            // txtAutor
+            // 
+            this.txtAutor.Location = new System.Drawing.Point(130, 46);
+            this.txtAutor.Name = "txtAutor";
+            this.txtAutor.Size = new System.Drawing.Size(162, 20);
+            this.txtAutor.TabIndex = 5;
+            // 
+            // txtCodigo
+            // 
+            this.txtCodigo.Location = new System.Drawing.Point(130, 78);
+            this.txtCodigo.Name = "txtCodigo";
+            this.txtCodigo.Size = new System.Drawing.Size(162, 20);
+            this.txtCodigo.TabIndex = 6;
+            // 
+            // btnAgregar
+            // 
+            this.btnAgregar.Location = new System.Drawing.Point(380, 14);
+            this.btnAgregar.Name = "btnAgregar";
+            this.btnAgregar.Size = new System.Drawing.Size(75, 23);
+            this.btnAgregar.TabIndex = 9;
+            this.btnAgregar.Text = "Agregar";
+            this.btnAgregar.UseVisualStyleBackColor = true;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(24, 116);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(99, 13);
+            this.label4.TabIndex = 7;
+            this.label4.Text = "Ha sido prestado?: ";
+            // 
+            // chkSi
+            // 
+            this.chkSi.AutoSize = true;
+            this.chkSi.Location = new System.Drawing.Point(130, 116);
+            this.chkSi.Name = "chkSi";
+            this.chkSi.Size = new System.Drawing.Size(36, 17);
+            this.chkSi.TabIndex = 10;
+            this.chkSi.Text = "SI";
+            this.chkSi.UseVisualStyleBackColor = true;
+            // 
+            // chkNo
+            // 
+            this.chkNo.AutoSize = true;
+            this.chkNo.Location = new System.Drawing.Point(172, 116);
+            this.chkNo.Name = "chkNo";
+            this.chkNo.Size = new System.Drawing.Size(42, 17);
+            this.chkNo.TabIndex = 11;
+            this.chkNo.Text = "NO";
+            this.chkNo.UseVisualStyleBackColor = true;
+            // 
+            // btnActualizar
+            // 
+            this.btnActualizar.Location = new System.Drawing.Point(380, 44);
+            this.btnActualizar.Name = "btnActualizar";
+            this.btnActualizar.Size = new System.Drawing.Size(75, 23);
+            this.btnActualizar.TabIndex = 12;
+            this.btnActualizar.Text = "Actualizar";
+            this.btnActualizar.UseVisualStyleBackColor = true;
+            this.btnActualizar.Click += new System.EventHandler(this.btnActualizar_Click);
+            // 
+            // FormDB
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.PeachPuff;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.btnActualizar);
+            this.Controls.Add(this.chkNo);
+            this.Controls.Add(this.chkSi);
+            this.Controls.Add(this.btnAgregar);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.txtCodigo);
+            this.Controls.Add(this.txtAutor);
+            this.Controls.Add(this.txtNombre);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.dataGridView1);
+            this.Name = "FormDB";
             this.Text = "FormDB";
+            this.Load += new System.EventHandler(this.FormDB_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox txtNombre;
+        private System.Windows.Forms.TextBox txtAutor;
+        private System.Windows.Forms.TextBox txtCodigo;
+        private System.Windows.Forms.Button btnAgregar;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.CheckBox chkSi;
+        private System.Windows.Forms.CheckBox chkNo;
+        private System.Windows.Forms.Button btnActualizar;
     }
 }
--- FormDB.cs
+++ FormDB.cs
@@ -7,14 +7,32 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Data.SqlClient;
 
 namespace MoansoS01
 {
     public partial class FormDB : Form
     {
+        SqlConnection conexionBD = new SqlConnection("server= localhost; database= LibreriaMoanso; integrated security= true");
         public FormDB()
         {
             InitializeComponent();
+        }
+
+        private void btnActualizar_Click(object sender, EventArgs e)
+        {
+            conexionBD.Open();
+            MessageBox.Show("La conexion a DB: " + conexionBD.Database + " ha sido exitosa");
+            conexionBD.Close();
+        }
+
+        private void FormDB_Load(object sender, EventArgs e)
+        {
+            string consulta = "select *from Libros";
+            SqlDataAdapter adaptador = new SqlDataAdapter(consulta, conexionBD);
+            DataTable dt = new DataTable();
+            adaptador.Fill(dt);
+            dataGridView1.DataSource = dt;
         }
     }
 }
